e9e03cf19172f059fca4dbd421a0ecb475133ea2
Abhishek Pandey <abhishek.pandey@ucertify.com>
2023-04-10 09:24:08 +0530

    first commit

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<!DOCTYPE HTML>
+<html>  
 <body>
-    
+
+<form action="welcome_get.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
 </body>
 </html>
